9d374a07589c0da1aa6495591e80f915a16fbfa6
pavva91 <valerio.mattioli@protonmail.com>
2023-03-31 13:19:51 +0200

    Add pretty print error handling

--- controllers/token.go
+++ controllers/token.go
@@ -1,11 +1,16 @@
 package controllers
 
 import (
+	"errors"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/pavva91/gin-gorm-rest/auth"
 	"github.com/pavva91/gin-gorm-rest/db"
+	"github.com/pavva91/gin-gorm-rest/errorhandling"
 	"github.com/pavva91/gin-gorm-rest/models"
-	"net/http"
+	"github.com/rs/zerolog/log"
 )
 
 type TokenRequest struct {
@@ -13,32 +18,44 @@ type TokenRequest struct {
 	Password string `json:"password"`
 }
 
-func GenerateToken(context *gin.Context) {
+func GenerateToken(c *gin.Context) {
 	var request TokenRequest
 	var user models.User
-	if err := context.ShouldBindJSON(&request); err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		context.Abort()
+	if err := c.ShouldBindJSON(&request); err != nil {
+		var verr validator.ValidationErrors
+		if errors.As(err, &verr) {
+			errorMessage := errorhandling.ValidationErrorsMessage{Message: errorhandling.NewJSONFormatter().Descriptive(verr)}
+			c.JSON(http.StatusBadRequest, errorMessage)
+			c.Abort()
+			return
+		}
+		log.Info().Err(err).Msg("unable to bind")
+		errorMessage := errorhandling.SimpleErrorMessage{Message: "Bad Request"}
+		c.JSON(http.StatusBadRequest, errorMessage)
+		c.Abort()
 		return
 	}
 	// check if email exists and password is correct
 	record := db.GetDB().Where("email = ?", request.Email).First(&user)
 	if record.Error != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"error": record.Error.Error()})
-		context.Abort()
+		errorMessage := errorhandling.SimpleErrorMessage{Message: record.Error.Error()}
+		c.JSON(http.StatusInternalServerError, errorMessage)
+		c.Abort()
 		return
 	}
 	credentialError := user.CheckPassword(request.Password)
 	if credentialError != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"error": "invalid credentials"})
-		context.Abort()
+		errorMessage := errorhandling.SimpleErrorMessage{Message: "Invalid Credentials"}
+		c.JSON(http.StatusUnauthorized, errorMessage)
+		c.Abort()
 		return
 	}
 	tokenString, err := auth.GenerateJWT(user.Email, user.Username)
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		context.Abort()
+		errorMessage := errorhandling.SimpleErrorMessage{Message: err.Error()}
+		c.JSON(http.StatusInternalServerError, errorMessage)
+		c.Abort()
 		return
 	}
-	context.JSON(http.StatusOK, gin.H{"token": tokenString})
+	c.JSON(http.StatusOK, gin.H{"token": tokenString})
 }
--- controllers/user.go
+++ controllers/user.go
@@ -1,16 +1,53 @@
 package controllers
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/pavva91/gin-gorm-rest/db"
+	"github.com/pavva91/gin-gorm-rest/errorhandling"
 	"github.com/pavva91/gin-gorm-rest/models"
+	"github.com/rs/zerolog/log"
 )
 
 type UserController struct{}
 
 var userModel = new(models.User)
+
+func RegisterUser(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		var verr validator.ValidationErrors
+		if errors.As(err, &verr) {
+			errorMessage := errorhandling.ValidationErrorsMessage{Message: errorhandling.NewJSONFormatter().Descriptive(verr)}
+			c.JSON(http.StatusBadRequest, errorMessage)
+			c.Abort()
+			return
+		}
+		log.Info().Err(err).Msg("unable to bind")
+		errorMessage := errorhandling.SimpleErrorMessage{Message: "Bad Request"}
+		c.JSON(http.StatusBadRequest, errorMessage)
+		c.Abort()
+		return
+	}
+	if err := user.HashPassword(user.Password); err != nil {
+		errorMessage := errorhandling.SimpleErrorMessage{Message: err.Error()}
+		c.JSON(http.StatusInternalServerError, errorMessage)
+		c.Abort()
+		return
+	}
+	record := db.GetDB().Create(&user)
+	if record.Error != nil {
+		errorMessage := errorhandling.SimpleErrorMessage{Message: record.Error.Error()}
+		c.JSON(http.StatusInternalServerError, errorMessage)
+		c.Abort()
+		return
+	}
+	// c.JSON(http.StatusCreated, gin.H{"userId": user.ID, "email": user.Email, "username": user.Username})
+	c.JSON(http.StatusCreated, &user)
+}
 
 func (u UserController) Retrieve(c *gin.Context) {
 	if c.Param("id") != "" {
@@ -40,32 +77,11 @@ func GetUser(c *gin.Context) {
 	c.JSON(200, &user)
 }
 
-func RegisterUser(c *gin.Context) {
-	var user models.User
-	if err := c.ShouldBindJSON(&user); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		c.Abort()
-		return
-	}
-	if err := user.HashPassword(user.Password); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		c.Abort()
-		return
-	}
-	record := db.GetDB().Create(&user)
-	if record.Error != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": record.Error.Error()})
-		c.Abort()
-		return
-	}
-	c.JSON(http.StatusCreated, gin.H{"userId": user.ID, "email": user.Email, "username": user.Username})
-}
-
 func CreateUser(c *gin.Context) {
 	var user models.User
 	c.BindJSON(&user)
 	db.GetDB().Create(&user)
-	c.JSON(200, &user)
+	c.JSON(http.StatusCreated, &user)
 }
 
 func DeleteUser(c *gin.Context) {
