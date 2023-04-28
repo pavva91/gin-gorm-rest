4d19fe734b41b8345254e0232c4eac80b3e6bdcf
pavva91 <valerio.mattioli@protonmail.com>
2023-04-28 16:32:06 +0200

    Moved all functions from EventModel to EventService

--- services/event.go
+++ services/event.go
@@ -3,7 +3,6 @@ package services
 import (
 	"github.com/pavva91/gin-gorm-rest/db"
 	"github.com/pavva91/gin-gorm-rest/models"
-	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -16,13 +15,39 @@ var (
 
 type eventService interface {
 	ListAllEvents() ([]models.Event, error)
+	CreateEvent(event *models.Event) (*models.Event, error)
+	GetById(id string) (*models.Event, error)
+	DeleteById(id string) (*models.Event, error)
+	SaveEvent(event *models.Event) (*models.Event, error)
 }
 
 type eventServiceImpl struct{}
 
 func (service eventServiceImpl) ListAllEvents() ([]models.Event, error) {
-	log.Info().Msg("I'm inside EventService")
 	var events []models.Event
 	db.GetDB().Find(&events)
 	return events, nil
 }
+
+func (service eventServiceImpl) CreateEvent(event *models.Event) (*models.Event, error) {
+	db.GetDB().Create(&event)
+	return event, nil
+}
+
+func (service eventServiceImpl) GetById(id string) (*models.Event, error) {
+	var event *models.Event
+	db.GetDB().Where("id = ?", id).First(&event)
+	return event, nil
+}
+
+func (service eventServiceImpl) DeleteById(id string) (*models.Event, error) {
+	var event *models.Event
+	db.GetDB().Where("id = ?", id).Delete(&event)
+	return event, nil
+}
+
+func (service eventServiceImpl) SaveEvent(event *models.Event) (*models.Event, error) {
+	db.GetDB().Save(&event)
+	return event, nil
+}
+
